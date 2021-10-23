53267de70ca20c0b8a6549d6f284e7bd9cb97614
Julia <jserranoarrese@gmail.com>
2021-10-23 14:22:09 +0200

    multiply & findBottom

---
+++ recursion/Explorador.java
@@ -0,0 +1,14 @@
+package aed.recursion;
+
+import es.upm.aedlib.Pair;
+import es.upm.aedlib.positionlist.*;
+
+
+public class Explorador {
+  
+	//solo bucles para explorar los caminos que salen de un  Lugar
+  public static Pair<Object,PositionList<Lugar>> explora(Lugar inicialLugar) {
+    return null;
+    //hacer
+  }
+}
